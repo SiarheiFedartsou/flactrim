0460ca94cbea6de8515e3451283c11568952b46b
Siarhei Fiedarcou <miksayer@gmail.com>
2010-06-04 12:38:36 +0300

    BitOStream

--- src/BitIStream.hpp
+++ src/BitIStream.hpp
@@ -8,8 +8,23 @@
 #include <stdint.h>
 #include "defines.hpp"
 
-class TypeOverflow : public std::exception {};
-class UnexpectedEOF : public std::exception {};
+class TypeOverflow: public std::exception 
+{
+public:
+	virtual const char* what() const throw()
+	{
+		return "Type overflow";
+	}
+};
+class UnexpectedEOF : public std::exception 
+{
+public:
+	virtual const char* what() const throw()
+	{
+		return "Unexpected end of file";
+	}
+
+};
 
 namespace Bytes
 {
